01a526da595b3d574cd6ca902cfdd6c08f38f177
errorcoder <kjw8971@naver.com>
2023-01-15 21:45:56 +0900

    Devtools and Query Keys

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,13 @@
+import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
 const instance = axios.create({
     baseURL : "http://127.0.0.1:8000/api/v3/"
 })
 
-export const getRooms = () => instance.get("rooms/").then(response => response.data);
+export const getRooms = () => instance.get("rooms/").then((response) => response.data);
 
-export const getRoom = () => instance.get(`rooms/9`).then(response => response.data);
+export const getRoom = ({queryKey} : QueryFunctionContext) => {
+    const [_, roomPk] = queryKey;
+    return instance.get(`rooms/${roomPk}`).then((response) => response.data);
+};
