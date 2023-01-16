acda4dedc12fa165850a9fc8cf4821a5b0956819
errorcoder <kjw8971@naver.com>
2023-01-16 17:28:19 +0900

    Review Part

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getRooms } from "../api";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
+import { IRoomList } from "../types";
 
 interface IPhoto {
     pk: string;
@@ -23,7 +24,7 @@ interface IRoom {
 }
 
 export default function Home() {
-    const { isLoading, data } = useQuery<IRoom[]>(["rooms"], getRooms);
+    const { isLoading, data } = useQuery<IRoomList[]>(["rooms"], getRooms);
     return ( 
         <Grid mt={10} px={{base:10, lg:40,}} columnGap={4} rowGap={8} templateColumns={{sm:"1fr", md:"1fr 1fr", lg:"repeat(3, 1fr)", xl:"repeat(4, 1fr)", "2xl" : "repeat(5, 1fr)",}}>
                 <Skeleton rounded="2xl" height={280} mb={7} />
