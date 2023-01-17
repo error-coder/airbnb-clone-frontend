4b29416d33317545c530441a104c992c01b2de5b
errorcoder <kjw8971@naver.com>
2023-01-17 16:51:40 +0900

    useUser #2

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getMe } from "../api";
 
 export default function useUser(){
-    const { isLoading, data, isError } = useQuery(['me'], getMe)
+    const { isLoading, data, isError } = useQuery(['me'], getMe, {retry: false});
     return {
         userLoading: isLoading,
         user: data,
