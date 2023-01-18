3343d908adae3175ba8da55e829ef77cf081c3c3
errorcoder <kjw8971@naver.com>
2023-01-18 17:31:44 +0900

    Github Login

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -10,7 +10,7 @@ export default function SocialLogin(){
                 <Divider />
             </HStack>
             <VStack>
-                <Button w="100%" leftIcon={<FaGithub />}>Continue with Github</Button>
+                <Button as="a" href="https://github.com/login/oauth/authorize?client_id=7295d1136e095e687640&scope=read:user,user:email" w="100%" leftIcon={<FaGithub />}>Continue with Github</Button>
                 <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>Continue with Kakao</Button>
             </VStack>
         </Box>
