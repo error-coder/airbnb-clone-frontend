a482118354d842a941d55fbc19f239c609acf288
errorcoder <kjw8971@naver.com>
2023-01-20 18:28:29 +0900

    Reack Hook Form Part

--- src/components/LogInModal.tsx
+++ src/components/LogInModal.tsx
@@ -1,4 +1,5 @@
-import { Input, Box, Button, Modal, ModalContent, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, VStack, InputGroup, InputLeftElement, } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { Input, Box, Button, Modal, ModalContent, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, VStack, InputGroup, InputLeftElement, Text, } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { FaUserNinja, FaLock } from "react-icons/fa";
 import SocialLogin from "./SocialLogin";
@@ -8,33 +9,25 @@ interface LogInModalProps {
     onClose:() => void;
 }
 
+interface IForm {
+    username: string;
+    password: string;
+}
+
+
 export default function LogInModal({ isOpen, onClose } : LogInModalProps){
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const onChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
-        const { name, value } = event.currentTarget;
-        if(name === "username"){
-            setUsername(value);
-        }else if(name === "password"){
-            setPassword(value);
-        }
-    };
-    const onSubmit = (event:React.SyntheticEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if(!email.includes("@")){
-            setEmailError("please write a valid email");
-        }
-        console.log(username, password);
-    };
-    return (
+    const { register, handleSubmit, formState: {errors} } = useForm<IForm>();
+    const onSubmit = (data: IForm) => {
+        console.log(data);
+    }
+    console.log(errors);
+        return (
         <Modal onClose={onClose} isOpen={isOpen}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Log in</ModalHeader>
                 <ModalCloseButton />
-                <ModalBody as="form" onSubmit={onSubmit as any}>
+                <ModalBody as="form" onSubmit={handleSubmit(onSubmit)}>
                     <VStack>
                         <InputGroup>
                         <InputLeftElement children={
@@ -42,11 +35,13 @@ export default function LogInModal({ isOpen, onClose } : LogInModalProps){
                                 <FaUserNinja />
                             </Box>
                         } />
-                            <Input required name="username" onChange={onChange} value={username} variant={"filled"} placeholder={"Username"} />
+                            <Input  {...register("username", {required:"Please write a username",})} required  variant={"filled"} placeholder={"Username"} />
+                            <Text fontSize={"sm"} color="red.500">{errors.username?.message}</Text>
                         </InputGroup>
                         <InputGroup>
                         <InputLeftElement children={ <Box color={"gray.500"}><FaLock /></Box>} />
-                            <Input required name="password" onChange={onChange} value={password} type="password" variant={"filled"} placeholder={"Password"} />
+                            <Input isInvalid={Boolean(errors.username?.message)} required {...register("password", {required:"Please write a password",})} type="password" variant={"filled"} placeholder={"Password"} />
+                            <Text fontSize={"sm"} color="red.500">{errors.password?.message}</Text>
                         </InputGroup>
                     </VStack>
                     <Button type="submit" mt={4} colorScheme={"red"} w="100%">Log in</Button>
