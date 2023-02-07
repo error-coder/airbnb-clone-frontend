f87ad07de947db108b4be6ba3a5907263797458e
errorcoder <kjw8971@naver.com>
2023-02-07 23:07:32 +0900

    Fixing

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -10,72 +10,165 @@ import {
     ModalContent,
     ModalHeader,
     ModalOverlay,
+    Select,
+    useToast,
     VStack,
-  } from "@chakra-ui/react";
-  import { FaUserNinja, FaLock, FaEnvelope, FaUserSecret } from "react-icons/fa";
-  import SocialLogin from "./SocialLogin";
-  
-  interface SignUpModalProps {
+} from '@chakra-ui/react';
+import { FaUserNinja, FaLock, FaEnvelope, FaUserSecret } from 'react-icons/fa';
+import SocialLogin from './SocialLogin';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { SignUp } from '../api';
+
+interface SignUpModalProps {
     isOpen: boolean;
     onClose: () => void;
-  }
-  
-  export default function SignUpModal({ isOpen, onClose }: SignUpModalProps) {
+}
+
+interface IForm {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+    currency: string;
+    gender: string;
+    language: string;
+}
+
+export default function SignUpModal({ isOpen, onClose }: SignUpModalProps) {
+    const { register, handleSubmit, reset } = useForm<IForm>();
+    const toast = useToast();
+    const queryClient = useQueryClient();
+    const mutation = useMutation(SignUp, {
+        onSuccess: () => {
+            toast({ title: 'Welcome!', status: 'success' });
+            onClose();
+            queryClient.refetchQueries(['me']);
+        },
+        onError: () => {
+            reset();
+        },
+    });
+
+    const onSubmit = ({
+        username,
+        password,
+        name,
+        email,
+        currency,
+        gender,
+        language,
+    }: IForm) => {
+        mutation.mutate({
+            username,
+            email,
+            name,
+            password,
+            currency,
+            gender,
+            language,
+        });
+    };
+
     return (
-      <Modal onClose={onClose} isOpen={isOpen}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Sign up</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <VStack>
-              <InputGroup>
-                <InputLeftElement
-                  children={
-                    <Box color="gray.500">
-                      <FaUserSecret />
-                    </Box>
-                  }
-                />
-                <Input variant={"filled"} placeholder="Name" />
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement
-                  children={
-                    <Box color="gray.500">
-                      <FaEnvelope />
-                    </Box>
-                  }
-                />
-                <Input variant={"filled"} placeholder="Email" />
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement
-                  children={
-                    <Box color="gray.500">
-                      <FaUserNinja />
-                    </Box>
-                  }
-                />
-                <Input variant={"filled"} placeholder="Username" />
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement
-                  children={
-                    <Box color="gray.500">
-                      <FaLock />
-                    </Box>
-                  }
-                />
-                <Input variant={"filled"} placeholder="Password" />
-              </InputGroup>
-            </VStack>
-            <Button mt={4} colorScheme={"red"} w="100%">
-              Log in
-            </Button>
-            <SocialLogin />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+        <Modal onClose={onClose} isOpen={isOpen}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Sign up</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody as="form" onSubmit={handleSubmit(onSubmit)}>
+                    <VStack>
+                        <InputGroup>
+                            <InputLeftElement
+                                children={
+                                    <Box color="gray.500">
+                                        <FaUserSecret />
+                                    </Box>
+                                }
+                            />
+                            <Input
+                                {...register('name', { required: true })}
+                                placeholder="name"
+                                variant="filled"
+                            />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputLeftElement
+                                children={
+                                    <Box color="gray.500">
+                                        <FaEnvelope />
+                                    </Box>
+                                }
+                            />
+                            <Input
+                                {...register('email', { required: true })}
+                                placeholder="email"
+                                variant="filled"
+                            />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputLeftElement
+                                children={
+                                    <Box color="gray.500">
+                                        <FaUserNinja />
+                                    </Box>
+                                }
+                            />
+                            <Input
+                                {...register('username', { required: true })}
+                                placeholder="username"
+                                variant="filled"
+                            />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputLeftElement
+                                children={
+                                    <Box color="gray.500">
+                                        <FaLock />
+                                    </Box>
+                                }
+                            />
+                            <Input
+                                {...register('password', { required: true })}
+                                placeholder="password"
+                                variant="filled"
+                                type="password"
+                            />
+                        </InputGroup>
+                        <Select
+                            placeholder="currency option"
+                            {...register('currency', { required: true })}
+                        >
+                            <option value="won">Korean Won</option>
+                            <option value="usd">Dollar</option>
+                        </Select>
+                        <Select
+                            placeholder="gender option"
+                            {...register('gender', { required: true })}
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </Select>
+                        <Select
+                            placeholder="language option"
+                            {...register('language', { required: true })}
+                        >
+                            <option value="kr">Korean</option>
+                            <option value="en">English</option>
+                        </Select>
+                    </VStack>
+                    <Button
+                        isLoading={mutation.isLoading}
+                        w="full"
+                        colorScheme="red"
+                        mt={4}
+                        type="submit"
+                    >
+                        Sign Up
+                    </Button>
+                    <SocialLogin />
+                </ModalBody>
+            </ModalContent>
+        </Modal>
     );
-  }
+}
--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,39 +1,42 @@
-import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
-import { useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { githubLogIn } from "../api";
+import { Heading, Spinner, Text, useToast, VStack } from '@chakra-ui/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { githubLogIn } from '../api';
 
 export default function GithubConfirm() {
-  const { search } = useLocation();
-  const toast = useToast();
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-  const confirmLogin = async () => {
-    const params = new URLSearchParams(search);
-    const code = params.get("code");
-    if (code) {
-      const status = await githubLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!",
-          position: "bottom-right",
-          description: "Happy to have you back!",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
-    }
-  };
-  useEffect(() => {
-    confirmLogin();
-  }, []);
-  return (
-    <VStack justifyContent={"center"} mt={40}>
-      <Heading>Processing log in...</Heading>
-      <Text>Don't go anywhere.</Text>
-      <Spinner size="lg" />
-    </VStack>
-  );
+    const { search } = useLocation();
+    const toast = useToast();
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const mutation = useMutation(githubLogIn, {
+        onSuccess: () => {
+            toast({
+                status: 'success',
+                title: 'Welcome!',
+                description: 'Happy to have you back!',
+            });
+
+            queryClient.refetchQueries(['me']);
+
+            navigate('/');
+        },
+    });
+    const confirmLogin = async () => {
+        const code = new URLSearchParams(search).get('code');
+        if (code) {
+            mutation.mutate(code);
+        }
+    };
+    useEffect(() => {
+        confirmLogin();
+    }, []);
+
+    return (
+        <VStack justifyContent={'center'} mt={40}>
+            <Heading>Processing log in...</Heading>
+            <Text>Don't go anywhere.</Text>
+            <Spinner size="lg" />
+        </VStack>
+    );
 }
